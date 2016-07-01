c18e8dcdcfcf99d015b91a6ff3fff1cd98f9a820
s15009 <s15009@std.it-college.ac.jp>
2016-07-01 11:18:05 +0900

    question update

--- skil_check/basic.py
+++ skil_check/basic.py
@@ -1,33 +1,33 @@
-
+# 自分の学科名　学年　学籍番号　を出力
 def print_self_information():
     print("ITスペシャリスト")
     print("２年")
     print("s15009")
-
+# 自分が８０歳になるまでの年数を出力
 def print_how_many_years_to_80():
     age = 19
     set = 80
     answer = set - age
     print(answer)
-
+# 与えられ得た値が奇数か偶数かを出力
 def print_odd_or_even(target):
     if(target % 2 == 0):
         print("偶数")
     else:
         print("奇数")
-
+# randomモジュールを使用して０，から５０までを生成し２５が出るまでほげと出力
 def print_hoge():
     from random import randint
     while(True):
         num = randint(0,50)
         if(num == 25):
-            print("hoge")
+            print("ほげ")
             break
         else:
             print(num)
-
+#100から1000までの偶数のみを表示
 def print_even_from_100_to_1000():
-    for x in range(100,1000):
+    for x in range(100,1000 + 1):
         if(x % 2 == 0):
             print(x)
         else:
